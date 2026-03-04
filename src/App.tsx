1bfe28d132d6bb47f797afd7656e983ff10853a4
kangluo345-sys <kangluo345@gmail.com>
2026-03-04 17:37:49 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -456,7 +456,7 @@ function App() {
         <div className="w-4/5 mx-auto">
           <div className="p-4 flex flex-row gap-8 rounded-3xl border-4" style={{minHeight: '450px', borderColor: '#B9CBDC'}}>
             <div className="w-5/6 grid grid-cols-2 gap-4">
-              <div className="aspect-[3/4] rounded-2xl overflow-hidden" >
+              <div className="aspect-[3/4.3] rounded-2xl overflow-hidden" >
                 <ImageCompareSlider
                   beforeImage="/68744e766a6b63d88f86d714366bcd31.jpg"
                   afterImage="/24a1e5e820d6d721e7aa3970ae648b43.jpg"
@@ -465,7 +465,7 @@ function App() {
                   initialPosition={50}
                 />
               </div>
-              <div className="aspect-[3/4] rounded-2xl overflow-hidden" >
+              <div className="aspect-[3/4.3] rounded-2xl overflow-hidden" >
                 <ImageCompareSlider
                   beforeImage="/5ef6dae25777256ca4e3fafedffbbea9.jpg"
                   afterImage="/c324214cb2a62d16d00510d9652c0f60.jpg"
@@ -494,9 +494,9 @@ function App() {
       {/* 移动端服务展示 */}
       <section className="md:hidden py-8">
         <div className="w-full px-1">
-          <div className="p-2 flex flex-col gap-3 rounded-2xl border-[3px]" style={{borderColor: '#B9CBDC'}}>
+          <div className="p-1 flex flex-col gap-3 rounded-2xl border-2" style={{borderColor: '#B9CBDC'}}>
             <div className="flex flex-col gap-3">
-              <div className="aspect-[3/4.2] rounded-lg overflow-hidden">
+              <div className="aspect-[3/4] rounded-lg overflow-hidden">
                 <ImageCompareSlider
                   beforeImage="/68744e766a6b63d88f86d714366bcd31.jpg"
                   afterImage="/24a1e5e820d6d721e7aa3970ae648b43.jpg"
@@ -505,7 +505,7 @@ function App() {
                   initialPosition={50}
                 />
               </div>
-              <div className="aspect-[3/4.2] rounded-lg overflow-hidden">
+              <div className="aspect-[3/4] rounded-lg overflow-hidden">
                 <ImageCompareSlider
                   beforeImage="/5ef6dae25777256ca4e3fafedffbbea9.jpg"
                   afterImage="/c324214cb2a62d16d00510d9652c0f60.jpg"
